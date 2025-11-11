b9fa9f25e6e24e979435ff1c8becd93c167e56d5
Satenik <saten914@gmail.com>
2025-11-11 19:08:18 +0100

    refactor(routes): mount /emails routes under /api

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
+import emailRoutes from "./email.routes.js";
 
 const api = Router();
 api.use("/auth", authRoutes);
+api.use("/emails", emailRoutes);
 
 export default api;
